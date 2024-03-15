e2977e953915b88e54e5e8423af9a9875266b04c
Ramón Armero Pardo <ramonarmeropardo@gmail.com>
2024-03-15 18:43:38 +0100

    Sustitución de las botoneras de view y card por un componente botonera

--- src/app/pages/user-view/user-view.component.ts
+++ src/app/pages/user-view/user-view.component.ts
@@ -1,12 +1,13 @@
 import { Component, inject } from '@angular/core';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { IUser } from '../../interfaces/iuser.interfaces';
 import { UsersService } from '../../services/users.service';
+import { BotoneraComponent } from '../../components/botonera/botonera.component';
 
 @Component({
   selector: 'app-user-view',
   standalone: true,
-  imports: [RouterLink],
+  imports: [BotoneraComponent],
   templateUrl: './user-view.component.html',
   styleUrl: './user-view.component.css',
 })
