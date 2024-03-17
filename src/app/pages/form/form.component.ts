2b58bdda298051dae58ae90a187ee8042e672faa
Ramón Armero Pardo <ramonarmeropardo@gmail.com>
2024-03-17 20:38:50 +0100

    Inclusión de los validadores para los distintos campos del formulario

--- src/app/pages/form/form.component.ts
+++ src/app/pages/form/form.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -16,16 +21,42 @@ export class FormComponent {
   constructor() {
     this.usersForm = new FormGroup(
       {
-        first_name: new FormControl('', []),
-        last_name: new FormControl('', []),
-        email: new FormControl('', []),
-        image: new FormControl('', []),
+        first_name: new FormControl('', [
+          Validators.required,
+          Validators.pattern(/\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+/),
+          // Procedencia de la expresión regular: https://regexr.com/3f8cm
+        ]),
+        last_name: new FormControl('', [
+          Validators.required,
+          Validators.pattern(/\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+/),
+          // Procedencia de la expresión regular: https://regexr.com/3f8cm
+        ]),
+        email: new FormControl('', [
+          Validators.required,
+          Validators.pattern(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/),
+          // Esta la vimos en clase
+        ]),
+        image: new FormControl('', [
+          Validators.required,
+          Validators.pattern(/^(https?|chrome):\/\/[^\s$.?#].[^\s]*$/),
+          // Procedencia de la expresión regular: https://regexr.com/3um70
+        ]),
       },
       []
     );
   }
 
   getDataForm() {
-    console.log(this.usersForm.value);
+    this.usersForm.reset();
+  }
+
+  checkControl(
+    formControlName: string,
+    validador: string
+  ): boolean | undefined {
+    return (
+      this.usersForm.get(formControlName)?.hasError(validador) &&
+      this.usersForm.get(formControlName)?.touched
+    );
   }
 }
